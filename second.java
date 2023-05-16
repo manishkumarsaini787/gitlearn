7e484a761738bedc2f6bd4961a24f8c9e7247780
manishkumarsaini787 <manishkumarsaini787@gmail.com>
2023-05-16 15:45:54 +0530

    first commit

---
+++ second.java
@@ -0,0 +1,9 @@
+class second{
+
+public static void main (String args[]){
+int a=12;
+int b=11;
+System.out.println("addition of two numbers are "+ (a+b));
+}
+}
+//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
